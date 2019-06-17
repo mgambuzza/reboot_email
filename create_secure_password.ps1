b69e3d0888edd604115bb899a477245488be2e3e
mgambuzza <51896452+mgambuzza@users.noreply.github.com>
2019-06-17 13:00:54 +1000

    Add files via upload
    create_secure_password.ps1 is used to create a file with encrypted password instead of plain text.

---
+++ create_secure_password.ps1
@@ -0,0 +1,6 @@
+﻿#convert your plain text password to secure credentials
+#when run, will pop up asking for your password, and will then create output file in specified folder, eg
+#read-host -assecurestring | convertfrom-securestring | out-file C:\scripts\email@account.onmicrosoft.com.txt
+#will create a txt file in c:\scripts with password as an encrypted string
+#replace foldername and office365emailaccount with your required parameters.
+read-host -assecurestring | convertfrom-securestring | out-file C:\foldername\office365emailaccount.txt
